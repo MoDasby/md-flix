0e08b453c5dc5584ccebb123a2cde84aceff8af2
giulliano <giullianomendes033@gmail.com>
2022-04-07 13:39:31 -0300

    estilização no componente Button

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,25 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ComponentStyleConfig } from '@chakra-ui/react';
 
-const config = {
-    initialColorMode: 'dark',
-    useSystemColorMode: true,
+const Button: ComponentStyleConfig = {
+    variants: {
+        outline: {
+            border: '2px solid',
+            borderColor: 'purple.500',
+            transition: '.5s',
+            _hover: {
+                bg: 'purple.500',
+            }
+        },
+    }
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+    initialColorMode: 'dark',
+    useSystemColorMode: true,
+    components: {
+        Button,
+    }
+});
 
 
 export default theme;
