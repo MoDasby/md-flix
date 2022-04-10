877c48d6d239f7904c0a67ec885df4dc512575b4
giulliano <giullianomendes033@gmail.com>
2022-04-10 12:10:52 -0300

    correções de bugs

--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -1,23 +1,62 @@
-const genres = {
-    28: "Ação",
-    12: "Aventura",
-    16: "Animação",
-    35: "Comédia",
-    80: "Crime",
-    99: "Documentário",
-    18: "Drama",
-    10751: "Família",
-    14: "Fantasia",
-    36: "História",
-    27: "Terror",
-    10402: "Música",
-    9648: "Mistério",
-    10749: "Romance",
-    878: "Ficção científica",
-    10770: "Cinema TV",
-    53: "Thriller",
-    10752: "Guerra",
-    37: "Faroeste",
-}
+const genres = [
+    {
+        id: 28,
+        name: "Ação"
+    }, {
+        id: 12,
+        name: "Aventura"
+    }, {
+        id: 16,
+        name: "Animação"
+    }, {
+        id: 35,
+        name: "Comédia"
+    }, {
+        id: 80,
+        name: "Crime"
+    }, {
+        id: 99,
+        name: "Documentário"
+    }, {
+        id: 18,
+        name: "Drama"
+    }, {
+        id: 10751,
+        name: "Família"
+    }, {
+        id: 14,
+        name: "Fantasia"
+    }, {
+        id: 36,
+        name: "História"
+    }, {
+        id: 27,
+        name: "Terror"
+    }, {
+        id: 10402,
+        name: "Música"
+    }, {
+        id: 9648,
+        name: "Mistério"
+    }, {
+        id: 10749,
+        name: "Romance"
+    }, {
+        id: 878,
+        name: "Ficção Científica"
+    }, {
+        id: 10770,
+        name: "Cinema TV"
+    }, {
+        id: 53,
+        name: "Thriller"
+    }, {
+        id: 10752,
+        name: "Guerra"
+    }, {
+        id: 37,
+        name: "Faroeste"
+    }
+]
 
 export default genres;
