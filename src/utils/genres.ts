e6de2fa53f36bdb20f4a09bba09c5cfa3fe434f3
giulliano <giullianomendes033@gmail.com>
2023-03-25 15:13:32 -0300

    add new genres

--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -1,61 +1,103 @@
 const genres = [
     {
-        id: 28,
-        name: "Ação"
-    }, {
-        id: 12,
-        name: "Aventura"
-    }, {
-        id: 16,
-        name: "Animação"
-    }, {
-        id: 35,
-        name: "Comédia"
-    }, {
-        id: 80,
-        name: "Crime"
-    }, {
-        id: 99,
-        name: "Documentário"
-    }, {
-        id: 18,
-        name: "Drama"
-    }, {
-        id: 10751,
-        name: "Família"
-    }, {
-        id: 14,
-        name: "Fantasia"
-    }, {
-        id: 36,
-        name: "História"
-    }, {
-        id: 27,
-        name: "Terror"
-    }, {
-        id: 10402,
-        name: "Música"
-    }, {
-        id: 9648,
-        name: "Mistério"
-    }, {
-        id: 10749,
-        name: "Romance"
-    }, {
-        id: 878,
-        name: "Ficção Científica"
-    }, {
-        id: 10770,
-        name: "Cinema TV"
-    }, {
-        id: 53,
-        name: "Thriller"
-    }, {
-        id: 10752,
-        name: "Guerra"
-    }, {
-        id: 37,
-        name: "Faroeste"
+        "id": 10759,
+        "name": "Ação e Aventura"
+    },
+    {
+        "id": 16,
+        "name": "Animação"
+    },
+    {
+        "id": 35,
+        "name": "Comédia"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentário"
+    },
+    {
+        "id": 18,
+        "name": "Drama"
+    },
+    {
+        "id": 10751,
+        "name": "Família"
+    },
+    {
+        "id": 10762,
+        "name": "Crianças"
+    },
+    {
+        "id": 9648,
+        "name": "Mistério"
+    },
+    {
+        "id": 10763,
+        "name": "Notícias"
+    },
+    {
+        "id": 10764,
+        "name": "Realidade"
+    },
+    {
+        "id": 10765,
+        "name": "Ficção Científica e Fantasia"
+    },
+    {
+        "id": 10766,
+        "name": "Telenovela"
+    },
+    {
+        "id": 10767,
+        "name": "Talk Show"
+    },
+    {
+        "id": 10768,
+        "name": "Guerra e Política"
+    },
+    {
+        "id": 37,
+        "name": "Faroeste"
+    },
+    {
+        "id": 14,
+        "name": "Fantasia"
+    },
+    {
+        "id": 36,
+        "name": "História"
+    },
+    {
+        "id": 27,
+        "name": "Terror"
+    },
+    {
+        "id": 10402,
+        "name": "Música"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Ficção Científica"
+    },
+    {
+        "id": 10770,
+        "name": "Filme para TV"
+    },
+    {
+        "id": 53,
+        "name": "Suspense"
+    },
+    {
+        "id": 10752,
+        "name": "Guerra"
     }
 ]
 
